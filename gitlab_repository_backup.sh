2ae4d3502ae155a05447e126a3efad570caf17ba
forestery <forestery@gmail.com>
2020-04-15 17:18:51 +0800

    add wiki data

--- gitlab_repository_backup.sh
+++ gitlab_repository_backup.sh
@@ -7,6 +7,8 @@ cat all | json_pp | grep ssh_url_to_repo | cut -d'"' -f 4 | sort | uniq | xargs 
 
 
 #cat all | json_pp | grep ssh_url_to_repo | cut -d'"' -f 4 | sort | uniq | xargs -I % echo 'git clone --mirror  %  %' | awk '{split($5,a,":");print $1" "$2" "$3" "$4" "a[2]; }' | awk '{ gsub("/",".",$5); print $1" "$2" "$3" "$4" "$5"/.git" ;}' | bash
+#wiki
+#cat all | json_pp | grep ssh_url_to_repo | cut -d'"' -f 4 | sort | uniq | awk '{print substr($1,1,length($1)-3)"wiki.git"  }'  | xargs -I % echo 'git clone --mirror  %  %'  | awk '{split($5,a,":");print $1" "$2" "$3" "$4" "a[2]; }' | awk '{ gsub("/",".",$5); print $1" "$2" "$3" "$4" "$5"/.git" ;}' > all2
 
 # config
 for d in ./*/ ; do (cd "$d" && git config --unset core.bare && git checkout master ); done
